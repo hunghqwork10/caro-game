1776affb5bc1aa823a076ef9703e2e9c33dcf584
Hung Hoang <hunghq.work@gmail.com>
2023-11-30 00:54:00 +0700

    Add buttons styles

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -8,6 +8,13 @@ MainMenu::MainMenu(QWidget* parent) : QWidget(parent)
     QPushButton* playButton = new QPushButton("Play");
     QPushButton* exitButton = new QPushButton("Exit");
 
+    playButton->setStyleSheet(
+        "QPushButton { background-color: #2ecc71; color: white; font-size: 20px; } QPushButton:hover { background-color: #27ae60; }"
+        );
+    exitButton->setStyleSheet(
+        "QPushButton { background-color: #e47c3c; color: white; font-size: 20px; } QPushButton:hover { background-color: #c0392b; }"
+        );
+
     QVBoxLayout* layout = new QVBoxLayout;
     layout->addWidget(playButton);
     layout->addWidget(exitButton);
