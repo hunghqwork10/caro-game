b1a022aa405813618b074ea7f04a876040623bbd
Hung Hoang <hunghq.work@gmail.com>
2023-12-08 00:57:55 +0700

    Add score label

--- game.cpp
+++ game.cpp
@@ -1,6 +1,8 @@
 #include "game.h"
 #include "square.h"
 #include "global.h"
+#include <QGraphicsProxyWidget>
+#include <QLabel>
 
 Game::Game(QWidget* parent) : QGraphicsView(parent)
 {
@@ -11,8 +13,27 @@ Game::Game(QWidget* parent) : QGraphicsView(parent)
 
     size = 30;
 
+    // Tạo bảng tỉ số
+    // tao QLabel
+    QLabel *scoreLabel = new QLabel(this);
+    scoreLabel->setGeometry(QRect(QPoint(50, 0), QSize(200, 50)));
+
+    // Giả sử ban đầu tỉ số là 0-0
+    int scorePlayer1 = 0;
+    int scorePlayer2 = 0;
+
+    // Cập nhật tỉ số lên QLabel
+    QString scoreText = "Player 1: " + QString::number(scorePlayer1) + " - Player 2: " + QString::number(scorePlayer2);
+    scoreLabel->setText(scoreText);
+
+    // thêm QLabel vào QGraphicsScene
+    QGraphicsProxyWidget *proxy = scene->addWidget(scoreLabel);
+
     // ve ban co
     drawBoard();
+
+    // đặt kích thước cố định cho cửa sổ game
+    setFixedSize(560, 560);
 
     // hien thi QGraphicsScene
 //    show();
